2b410fa1cabf0caa53f9d2d1d150aa48aca63ba1
Yuval <yuvald@montopay.com>
2024-01-21 16:45:08 +0200

    insert demo data

---
+++ pages/example.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ExamplePage() {
+  return (
+    <div>Example Page</div>
+  )
+}
---
+++ tableData.tsx
@@ -0,0 +1,912 @@
+export 
+const tableData = [
+  {
+    id: 1,
+    name: "Daniel",
+    lastName: "Brito",
+  },
+  {
+    id: 2,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 3,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 4,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 5,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 6,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 7,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 8,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 9,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 10,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 11,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 12,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 13,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 14,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 15,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 16,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 17,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 18,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 19,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 20,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 21,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 22,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 23,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 24,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 25,
+    name: "Jane",
+    lastName: "Smith",
+  }, {
+    id: 26,
+    name: "Daniel",
+    lastName: "Brito",
+  },
+  {
+    id: 27,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 28,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 29,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 30,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 31,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 32,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 33,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 34,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 35,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 36,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 37,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 38,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 39,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 40,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 41,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 42,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 43,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 44,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 45,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 46,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 47,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 48,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 49,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 50,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 51,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 52,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 53,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 54,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 55,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 56,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 57,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 58,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 59,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 60,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 61,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 62,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 63,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 64,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 65,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 66,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 67,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 68,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 69,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 70,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 71,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 72,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 73,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 74,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 75,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 76,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 77,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 78,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 79,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 80,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 81,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 82,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 83,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 84,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 85,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 86,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 87,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 88,
+    name: "Jane",
+    lastName: "Doe",
+  },
+  {
+    id: 89,
+    name: "John",
+    lastName: "Smith",
+  },
+  {
+    id: 90,
+    name: "Jane",
+    lastName: "Smith",
+  },
+  {
+    id: 91,
+    name: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 92,
+    name: "Jane",
+    lastName: "Doe",
+  },
+    {
+        id: 93,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 94,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 95,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 96,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 97,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 98,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 99,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 100,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 101,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 102,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 103,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 104,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 105,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 106,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 107,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 108,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 109,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 110,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 111,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 112,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 113,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 114,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 115,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 116,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 117,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 118,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 119,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 120,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 121,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 122,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 123,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 124,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 125,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 126,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 127,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 128,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 129,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 130,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 131,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 132,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 133,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 134,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 135,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 136,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 137,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 138,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 139,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 140,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 141,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 142,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 143,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 144,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 145,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 146,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 147,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 148,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 149,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 150,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 151,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 152,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 153,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 154,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 155,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 156,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 157,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 158,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 159,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 160,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 161,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 162,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 163,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 164,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 165,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 166,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 167,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 168,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 169,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 170,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 171,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 172,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 173,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 174,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 175,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 176,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 177,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 178,
+        name: "Jane",
+        lastName: "Smith",
+    },
+    {
+        id: 179,
+        name: "John",
+        lastName: "Doe",
+    },
+    {
+        id: 180,
+        name: "Jane",
+        lastName: "Doe",
+    },
+    {
+        id: 181,
+        name: "John",
+        lastName: "Smith",
+    },
+    {
+        id: 182,
+        name: "Jane",
+        lastName: "Smith",
+    },
+];
